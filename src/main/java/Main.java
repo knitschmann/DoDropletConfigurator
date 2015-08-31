f02e97a06aa755a849298b988b6280e33a98cbcb
kevin <knitschmann@mailbox.tu-berlin.de>
2015-08-31 18:45:26 +0200

    created several error msg for already active servers, already deleted server etc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,9 @@ import com.myjeeva.digitalocean.impl.DigitalOceanClient;
 import com.myjeeva.digitalocean.pojo.*;
 import org.apache.log4j.BasicConfigurator;
 
+import java.awt.datatransfer.*;
+import java.awt.Toolkit;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -17,127 +20,129 @@ public class Main {
     static final boolean debug = false;
 
     public static void main(String args[]) throws Exception {
-        String authToken = "$TOKEN";
+        String authToken = "c7956ff49aa22c355eab8bea8489dbc94b17c26a29da686bbb09e35f629bab1f";
         DigitalOcean apiClient = new DigitalOceanClient(authToken);
 
 
-        BasicConfigurator.configure();
+        //BasicConfigurator.configure();
 
         Scanner scanner = new Scanner(System.in);
         displayGUI();
         checkAuthToken(authToken);
-        while(scanner.hasNext()){
-            int input = scanner.nextInt();
-            switch(input){
-                case 1:
-                    System.out.println("Creating new Mumble Server droplet...");
-                    createMumbleServer(apiClient);
-                    getIpAddress(apiClient);
-                    displayGUISpace();
-                    displayGUI();
-                    break;
-                case 2:
-                    System.out.println("Fetching IP address for mumble Server...");
-                    getIpAddress(apiClient);
-                    displayGUISpace();
-                    displayGUI();
-                    break;
-                case 3:
-                    System.out.println("Deleting Mumble Server droplet...");
-                    deleteMumbleDroplet(apiClient);
-                    displayGUISpace();
-                    displayGUI();
-                    break;
-                case 4:
-                    System.out.println("Bye bye");
-                    scanner.close();
-                    System.exit(0);
+        while (scanner.hasNext()) {
+            try {
+                if (!scanner.hasNextInt()) {
+                    System.out.println("Type in the number of your choosing [1 - 4]");
+                    scanner.nextLine();
+                } else {
+                    int input = scanner.nextInt();
+                    switch (input) {
+                        case 1:
+                            System.out.println("Creating new Mumble Server droplet...");
+                            createMumbleServer(apiClient);
+                            getIpAddress(apiClient);
+                            displayGUISpace();
+                            displayGUI();
+                            break;
+                        case 2:
+                            System.out.println("Fetching IP address for mumble Server...");
+                            getIpAddress(apiClient);
+                            displayGUISpace();
+                            displayGUI();
+                            break;
+                        case 3:
+                            System.out.println("Deleting Mumble Server droplet...");
+                            deleteMumbleDroplet(apiClient);
+                            displayGUISpace();
+                            displayGUI();
+                            break;
+                        case 4:
+                            System.out.println("by Kevin Nitschmann");
+                            scanner.close();
+                            System.exit(0);
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("Please enter a number!");
             }
         }
-
-//        createMumbleServer(apiClient);
-//        getIpAddress(apiClient);
-
-
-
-
-
-
-
     }
 
-    private static void checkAuthToken(String authToken) {
-        if(authToken.equals("$TOKEN")){
-            System.out.println("WARNING: Authenticator Token has not been set");
+
+    private static void deleteMumbleDroplet(DigitalOcean apiClient) throws DigitalOceanException, RequestUnsuccessfulException {
+        Droplet mumbleDroplet = getAvailableDroplets(apiClient);
+        if (mumbleDroplet == null) {
+            System.out.println("There is no active Mumble Server");
+        } else {
+            apiClient.deleteDroplet(mumbleDroplet.getId());
         }
     }
 
-    private static void displayGUISpace() {
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-    }
-
-    private static void displayGUI() {
-        System.out.println("#################################################################");
-        System.out.println("Mumble Server Deployment Script");
-        System.out.println("1 - Create Droplet and Start Server from Snapshot");
-        System.out.println("2 - get IP address for active Mumble Server");
-        System.out.println("3 - destroy Server");
-        System.out.println("4 - exit");
-        System.out.println("#################################################################");
-    }
-
-    private static void deleteMumbleDroplet(DigitalOcean apiClient) throws DigitalOceanException, RequestUnsuccessfulException {
+    private static Droplet getAvailableDroplets(DigitalOcean apiClient) throws DigitalOceanException, RequestUnsuccessfulException {
         Droplets droplets = apiClient.getAvailableDroplets(0);
         List<Droplet> dropletList = droplets.getDroplets();
         Droplet mumbleDroplet = null;
         for (Droplet droplet : dropletList) {
-            if (droplet.getName().equals("api-client-test-host")) {
+            if (droplet.getName().equals("Mumble")) {
                 mumbleDroplet = droplet;
             }
         }
-        apiClient.deleteDroplet(mumbleDroplet.getId());
+        return mumbleDroplet;
     }
 
     private static void getIpAddress(DigitalOcean apiClient) throws DigitalOceanException, RequestUnsuccessfulException {
-        Droplets droplets = apiClient.getAvailableDroplets(0);
-        List<Droplet> dropletList = droplets.getDroplets();
-        Droplet mumbleDroplet = null;
-        for (Droplet droplet : dropletList) {
-            if (droplet.getName().equals("api-client-test-host")) {
-                mumbleDroplet = droplet;
-            }
-        }
-        String status;
-        DropletStatus dropletStatus = mumbleDroplet.getStatus();
-        status = dropletStatus.toString();
-        boolean waitForIP = true;
-        while (waitForIP) {
+        Droplet mumbleDroplet = getAvailableDroplets(apiClient);
+        if (mumbleDroplet == null) {
+            System.out.println("No active Mumble Server has been found");
 
-            try {
-                if (!mumbleDroplet.getStatus().toString().equals("active")) {
-                    System.out.println("Server seems to be inactive, trying again in a second ...");
-                    Thread.sleep(5000);
-                }else{
-                    //System.out.println("Server seems to be active now, fetching IP...");
-                    Networks networks = mumbleDroplet.getNetworks();
-                    List<Network> networksIP4 = networks.getVersion4Networks();
-                    Network ip4address = networksIP4.get(0);
-                    ip4address.getIpAddress();
-                    System.out.println(ip4address.getIpAddress());
-                    waitForIP = false;
+        } else {
+            String status;
+            status = mumbleDroplet.getStatus().toString();
+            boolean waitForIP = true;
+            while (waitForIP) {
+
+                try {
+                    if (!mumbleDroplet.isActive()) {
+                        System.out.println("Server still setting up, waiting for IP ...");
+                        Thread.sleep(5000);
+                        mumbleDroplet = getAvailableDroplets(apiClient);
+                        status = mumbleDroplet.getStatus().toString();
+
+                    } else {
+                        Networks networks = mumbleDroplet.getNetworks();
+                        List<Network> networksIP4 = networks.getVersion4Networks();
+                        Network ip4address = networksIP4.get(0);
+                        ip4address.getIpAddress();
+                        System.out.println(ip4address.getIpAddress());
+                        System.out.println("IP address has been copied to clipboard");
+                        String myString = ip4address.getIpAddress();
+                        StringSelection stringSelection = new StringSelection(myString);
+                        Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+                        clpbrd.setContents(stringSelection, null);
+                        waitForIP = false;
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
 
+            }
         }
     }
 
     private static void createMumbleServer(DigitalOcean apiClient) {
         Images userImages = null;
         try {
+
+            Droplets droplets = apiClient.getAvailableDroplets(0);
+            List<Droplet> dropletList = droplets.getDroplets();
+            Droplet mumbleDroplet = null;
+            for (Droplet droplet : dropletList) {
+                if (droplet.getName().equals("Mumble")) {
+                    System.out.println("Mumble server already started");
+                    return;
+                }
+            }
+
             userImages = apiClient.getUserImages(0);
             List<Image> userImagesList = userImages.getImages();
             Image mumbleImage = null;
@@ -160,9 +165,9 @@ public class Main {
         }
     }
 
-    public static Droplet createDroplet(DigitalOcean apiClient, Image img) {
+    private static Droplet createDroplet(DigitalOcean apiClient, Image img) {
         Droplet newDroplet = new Droplet();
-        newDroplet.setName("api-client-test-host");
+        newDroplet.setName("Mumble");
         newDroplet.setSize("512mb"); // setting size by slug value
         newDroplet.setRegion(new Region("fra1")); // setting region by slug value; sgp1 => Singapore 1 Data center
         newDroplet.setImage(img); // setting by Image Id 1601 => centos-5-8-x64 also available in image slug value
@@ -170,5 +175,28 @@ public class Main {
         newDroplet.setEnableIpv6(Boolean.FALSE);
         newDroplet.setEnablePrivateNetworking(Boolean.FALSE);
         return newDroplet;
+    }
+
+
+    private static void checkAuthToken(String authToken) {
+        if (authToken.equals("$TOKEN")) {
+            System.out.println("WARNING: Authenticator Token has not been set");
+        }
+    }
+
+    private static void displayGUISpace() {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+
+    private static void displayGUI() {
+        System.out.println("#################################################################");
+        System.out.println("Mumble Server Deployment Script");
+        System.out.println("1 - Create Droplet and Start Server from Snapshot");
+        System.out.println("2 - get IP address for active Mumble Server");
+        System.out.println("3 - destroy Server");
+        System.out.println("4 - exit");
+        System.out.println("#################################################################");
     }
 }
